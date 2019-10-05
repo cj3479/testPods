cd387a4e9eca86c9304613838ac6ff70d53b6d0c
tandytian <tandytian@tentcent.com>
2019-10-05 17:29:10 +0800

    dsfs

---
+++ Frameworks/TestDMlib2.framework/Headers/LibPerson.h
@@ -0,0 +1,20 @@
+//
+//  LibPerson.h
+//  testDMlib
+//
+//  Created by chengjian on 2019/6/3.
+//  Copyright © 2019 chengjian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LibPerson : NSObject
+//@property (nonatomic,copy) NSString *name;
+//-(void)watch;
+//-(void)eat;
+@end
+
+NS_ASSUME_NONNULL_END
+
