b7859c369457c1000f6e3ced3169d3baea92fc75
jackjcheng <jackjcheng@tencent.com>
2019-10-17 22:37:58 +0800

    11

---
+++ TestClass0.h
@@ -0,0 +1,16 @@
+//
+//  TestClass0.h
+//  Pods-testPods_Example
+//
+//  Created by chengjian on 2019/10/17.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestClass0 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestClass0.m
@@ -0,0 +1,12 @@
+//
+//  TestClass0.m
+//  Pods-testPods_Example
+//
+//  Created by chengjian on 2019/10/17.
+//
+
+#import "TestClass0.h"
+
+@implementation TestClass0
+
+@end
