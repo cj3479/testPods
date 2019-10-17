6689c88b3f28a6320e1c9ee2feb3a92df69b40ad
jackjcheng <jackjcheng@tencent.com>
2019-10-17 22:53:31 +0800

    dsf

---
+++ testPods/Classes0/TestPerson0.h
@@ -0,0 +1,16 @@
+//
+//  TestPerson.h
+//  Pods-testPods_Example
+//
+//  Created by chengjian on 2019/10/4.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestPerson0 : NSObject
+-(void)eat0;
+@end
+
+NS_ASSUME_NONNULL_END
